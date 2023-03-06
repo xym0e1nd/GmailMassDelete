e558181f23e9a47b9950d21e69c817293f53a300
Raffaele Pagano <rafpgn@gmail.com>
2023-03-06 11:06:51 +0100

    primo commit

---
+++ GmailMassDelete/Form1.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        TextBox3 = New TextBox()
+        Label4 = New Label()
+        TextBox4 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(50, 130)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "Avvia"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(217, 130)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "Ferma"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(135, 12)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(167, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(135, 45)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(43, 23)
+        TextBox2.TabIndex = 3
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(88, 15)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(37, 15)
+        Label1.TabIndex = 4
+        Label1.Text = "Stato:"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(93, 49)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(32, 15)
+        Label2.TabIndex = 5
+        Label2.Text = "Giro:"' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(8, 77)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(117, 15)
+        Label3.TabIndex = 6
+        Label3.Text = "Tot. email cancellate:"' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(135, 74)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(167, 23)
+        TextBox3.TabIndex = 7
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(187, 49)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(61, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Stato giro:"' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(254, 45)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(48, 23)
+        TextBox4.TabIndex = 9
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(339, 165)
+        Controls.Add(TextBox4)
+        Controls.Add(Label4)
+        Controls.Add(TextBox3)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Gmail Mass Delete"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ GmailMassDelete/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.ComponentModel
+Imports System.Runtime.InteropServices
+
+Public Class Form1
+    ' <DllImport("user32.dll")>
+    ' Private Shared Sub mouse_event(dwFlags As UInteger, dx As UInteger, dy As UInteger, dwData As UInteger, dwExtraInfo As Integer)
+    'End Sub
+
+    '  Declare Function mc Lib "user32.dll" Alias "mouse_event" (flag, x, y, button, extra)
+
+    Public Declare Auto Function SetCursorPos Lib "User32.dll" (ByVal X As Integer, ByVal Y As Integer) As Long
+    Public Declare Auto Function GetCursorPos Lib "User32.dll" (ByRef lpPoint As Point) As Long
+    Public Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Long, ByVal dx As Long, ByVal dy As Long, ByVal cButtons As Long, ByVal dwExtraInfo As Long)
+    Public Const MOUSEEVENTF_LEFTDOWN = &H2 ' left button down
+    Public Const MOUSEEVENTF_LEFTUP = &H4 ' left button up
+    Public Const MOUSEEVENTF_MIDDLEDOWN = &H20 ' middle button down
+    Public Const MOUSEEVENTF_MIDDLEUP = &H40 ' middle button up
+    Public Const MOUSEEVENTF_RIGHTDOWN = &H8 ' right button down
+    Public Const MOUSEEVENTF_RIGHTUP = &H10 ' right button up
+
+
+    ' Const MOUSEEVENTF_MOVE As Int32 = &H1 '  mouse move
+    ' Const MOUSEEVENTF_LEFTDOWN As Int32 = &H2 '  left button down
+    ' Const MOUSEEVENTF_LEFTUP As Int32 = &H4 '  left button up
+    ' Const MOUSEEVENTF_RIGHTDOWN As Int32 = &H8 '  right button down
+    ' Const MOUSEEVENTF_RIGHTUP As Int32 = &H10 '  right button up
+    ' Const MOUSEEVENTF_MIDDLEDOWN As Int32 = &H20 '  middle button down
+    ' Const MOUSEEVENTF_MIDDLEUP As Int32 = &H40 '  middle button up
+    ' Const MOUSEEVENTF_ABSOLUTE As Int32 = &H8000 '  absolute move
+    ' Const MOUSEEVENTF_WHEEL As Int32 = &H800 ' wheel button rolled
+
+    Public Fermati As Boolean = False
+    Public CheckBox1Value As Boolean = False
+    'Sub main()
+    '    Console.WriteLine("ciao")
+    '    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        System.Diagnostics.Debug.WriteLine("btn Avvia")
+        System.Diagnostics.Debug.WriteLine("sender=" & sender.ToString)
+        System.Diagnostics.Debug.WriteLine("e=" & e.ToString)
+        System.Diagnostics.Debug.WriteLine("Raffy")
+        REM Me.MousePosition.X = 10
+
+        Dim Giro As Integer = 0
+        Dim Azione As String = ""
+
+        Giro = 1
+        Azione = "SEL"
+        Do
+            ' inizio giro
+            TextBox2.Text = "" & Giro
+            TextBox4.Text = "Begin"
+
+            ' azione
+            If Azione = "SEL" Then
+                TextBox1.Text = "Seleziono..."
+                Cursor.Position = New System.Drawing.Point(350, 190)
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                Azione = "CANC"
+            ElseIf Azione = "CANC" Then
+                TextBox1.Text = "Cancello..."
+                TextBox3.Text = "" & (Giro * 100)
+                Cursor.Position = New System.Drawing.Point(500, 190)
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0)
+                mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0)
+                Azione = "SEL"
+            End If
+
+            ' pausa
+            Application.DoEvents()
+            Threading.Thread.Sleep(5000) ' 1000 milliseconds = 1.0 seconds
+
+            ' fine giro
+            System.Diagnostics.Debug.WriteLine("giro n° " & Giro)
+            TextBox4.Text = "End"
+            Application.DoEvents()
+            Threading.Thread.Sleep(1000) ' 1000 milliseconds = 1.0 seconds
+
+            ' Next
+            Giro += 1
+        Loop While Giro <= 1 Or Fermati = False
+
+        REM mouse_event(MOUSEEVENTF_LEFTDOWN, 20, 20, 0, 0)
+
+        REM Me.mouse.Cursor.Position.X = 1
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        System.Diagnostics.Debug.WriteLine("Carico")
+    End Sub
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Console.WriteLine("btn Ferma")
+        Fermati = True
+    End Sub
+End Class
